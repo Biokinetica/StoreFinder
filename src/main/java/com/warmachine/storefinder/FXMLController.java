98aa73079e145144b15cd8c56eae15b1ecfedcc1
brenton <brenton@DixieFlatline>
2014-08-17 19:06:05 -0400

    Making more headway. Got it showing multiple stores. Filtering by distance is next.

--- src/main/java/com/warmachine/storefinder/FXMLController.java
+++ src/main/java/com/warmachine/storefinder/FXMLController.java
@@ -1,14 +1,20 @@
 package com.warmachine.storefinder;
 
+import com.google.code.geocoder.Geocoder;
+import com.google.code.geocoder.GeocoderRequestBuilder;
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderRequest;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import java.io.IOException;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.Arrays;
-import java.util.Map;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -22,6 +28,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
 
 public class FXMLController implements Initializable {
     
@@ -66,35 +74,48 @@ public class FXMLController implements Initializable {
         
         MongoCredential creds = MongoCredential.createMongoCRCredential("User", "project", "WarmaHordes".toCharArray());
             mongoClient = new MongoClient(address, Arrays.asList(creds));
+            storeInfo = new BasicDBObject();
     }    
 
     @FXML
-    private void handleSearch(MouseEvent event) {
+    private void handleSearch(MouseEvent event) throws IOException {
         
         DBCollection colls = mongoClient.getDB("project").getCollection("Stores");
-        /*
-        storeInfo.append("Address", AddrLine.getText())
-                .append("City", CityLine.getText())
-                .append("Zip", ZipLine.getText());
-        */
         
-        Map stores = colls.findOne().toMap();
         
+        double coordinates[] = new double[2];
+            
+            final Geocoder geocoder = new Geocoder();
+         GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(AddrLine.getText() + " " + CityLine.getText() + ", MI " + ZipLine.getText() ).setLanguage("en").getGeocoderRequest();
+         GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
+
+            coordinates[0] = geocoderResponse.getResults().get(0).getGeometry().getLocation().getLng().doubleValue();
+            coordinates[1] = geocoderResponse.getResults().get(0).getGeometry().getLocation().getLat().doubleValue();
+            
+            storeInfo.put("loc",new BasicDBObject("$near", new BasicDBObject("$geometry", new BasicDBObject("type","Point").append("coordinates", coordinates))));
+
+        
+           List<DBObject> store = colls.find(storeInfo).toArray();
+        
+        
+        
+        for(DBObject s : store){
         GridPane pane = new GridPane();
         pane.addColumn(0, new Label("Address: "));
-        pane.addRow(0, new Label(stores.get("Address").toString()));
+        pane.addRow(0, new Label(s.get("Address").toString()));
         
         pane.addColumn(0, new Label("City: "));
-        pane.addRow(1, new Label(stores.get("City").toString()));
+        pane.addRow(1, new Label(s.get("City").toString()));
         
         pane.addRow(2, new Label("Zip: "));
-        pane.addRow(2, new Label(stores.get("Zip").toString()));
+        pane.addRow(2, new Label(s.get("Zip").toString()));
         
         pane.addRow(3, new Label("Phone: "));
-        pane.addRow(3, new Label(stores.get("Phone").toString()));
+        pane.addRow(3, new Label(s.get("Phone").toString()));
         
- TitledPane t3 = new TitledPane(colls.findOne().toMap().get("Store").toString(), pane );
+        TitledPane t3 = new TitledPane(s.get("Store").toString(), pane );
         Results.getPanes().add(t3);
+        }
         
     }
 }
